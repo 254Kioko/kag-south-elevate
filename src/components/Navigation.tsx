42de6f8ae4dcbfba73dd38ea70263998e8b4c53f
254Kioko <128481140+254Kioko@users.noreply.github.com>
2025-09-25 17:15:15 +0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,13 +8,14 @@ const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
 
-  const navItems = [
-    { name: "Home", href: "/" },
-    { name: "About Us", href: "/about-ministries" },
-    { name: "Contact", href: "/contact" },
-    { name: "Give Online", href: "/give-online" }
-     { name: "Admin", href: "/admin" } 
-  ];
+ const navItems = [
+  { name: "Home", href: "/" },
+  { name: "About Us", href: "/about-ministries" },
+  { name: "Contact", href: "/contact" },
+  { name: "Give Online", href: "/give-online" },
+  { name: "Admin", href: "/admin" } // 
+];
+
 
   const closeSheet = () => setIsOpen(false);
 
