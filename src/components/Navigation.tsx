f5554d631c34e525917baabdbed9770e2e63ca4c
254Kioko <128481140+254Kioko@users.noreply.github.com>
2025-10-19 08:11:51 +0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -12,6 +12,7 @@ const Navigation = () => {
   const navItems = [
     { name: "Home", href: "/" },
     { name: "About Us", href: "/about-ministries" },
+    { name: "Events", href: "/events" },
     { name: "Sermons", href: "/sermons" },
     { name: "Give", href: "/give-online" },
     { name: "Contact", href: "/contact" },
