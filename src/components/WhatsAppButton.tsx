75c3e9444f36110894098d2ff523acc826a129d9
254Kioko <128481140+254Kioko@users.noreply.github.com>
2025-12-29 17:18:15 +0300

    Update WhatsAppButton.tsx

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/2547XXXXXXXX" // replace with your number
+      href="https://wa.me/254724177832" // replace with your number
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-5 right-5 z-50 flex items-center justify-center
